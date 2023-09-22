43be415d87481936a9df63e43dab5c460e7a9336
tamimhossen <md.tamimhossen1767@gmail.com>
2023-09-22 20:52:52 +0600

    print function bug solved

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const tableBody = document.getElementById("table-body");
 let html = ''
 
 addBtn.addEventListener("click", () => {
-    html += '<tr>';
-    html += `<td> ${itemName.value} </td> <td> ${quantity.value} </td> <td> ${price.value} </td> <td> ${price.value * quantity.value} </td>`;
-    html += '</tr>'
-    tableBody.innerHTML = html;
+
+    if (itemName.value != '' && price.value != '') {
+        html += '<tr>';
+        html += `<td> ${itemName.value} </td> <td> ${quantity.value} </td> <td> ${price.value} </td> <td> ${price.value * quantity.value} </td>`;
+        html += '</tr>'
+        tableBody.innerHTML = html;
+        itemName.value = '';
+        price.value = '';
+        quantity.value = 1;
+    }else {
+        alert("Enter item and it's price");
+    }
 })
 
 document.getElementById("printBtn").addEventListener("click", () => {
